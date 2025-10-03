3395159c6fe67c7e0b904208f8caed1ed384191d
SafeyyyyY <safeyipotale@gmail.com>
2025-10-03 19:18:25 +0800

    add index.html

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const images = ["karyaindah.jpg", "karyaindah2.jpg", "karyaindah5.jpg","karyaindah6.jpg"];
+    let currentIndex = 0;
+    const banner = document.querySelector(".banner");
+
+    function changeBanner() {
+        currentIndex = (currentIndex + 1) % images.length;
+        banner.style.backgroundImage = `url(${images[currentIndex]})`;
+    }
+
+    setInterval(changeBanner, 4000);
+
+    const slides = document.querySelectorAll('.slogan li.slide');
+    let currentSlide = 0;
+
+    function nextSlide() {
+        slides[currentSlide].classList.remove('active');
+        slides.forEach(slide => slide.style.color = '');
+        currentSlide = (currentSlide + 1) % slides.length;
+        slides[currentSlide].classList.add('active');
+        if (currentSlide === 1) {
+            slides[currentSlide].style.color = 'rgb(231, 123, 46)';
+        }
+    }
+
+    const btn = document.getElementById("toggleBtn");
+    btn.addEventListener("click", function() {
+        document.body.classList.toggle("alt");
+        nextSlide();
+    });
+});
